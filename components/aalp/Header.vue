8ec202a39589755de980a20bcc44acd4629ca3d4
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-14 20:09:55 +0300

    feat: added header

---
+++ components/aalp/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <NuxtLink :to="{ name: 'index' }" class="flex items-center">
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            AALP
+          </span>
+        </NuxtLink>
+        <div class="flex items-center lg:order-2">
+          <NuxtLink href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+            Link
+          </NuxtLink>
+          <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+            <span class="sr-only">Menu</span>
+            <Icon name="tabler:menu-2" class="w-6 h-6" />
+          </button>
+        </div>
+        <div id="mobile-menu-2" class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
+          <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li>
+              <NuxtLink :to="{ name: 'index' }" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                Home
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <AHeader />
+
     <slot />
   </div>
 </template>
