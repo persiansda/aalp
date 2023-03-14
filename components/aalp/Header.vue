55618741f5420af5c42852a74dca9f19062c07ce
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-14 20:15:29 +0300

    feat: added logo

--- components/aalp/Header.vue
+++ components/aalp/Header.vue
@@ -3,9 +3,7 @@
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <NuxtLink :to="{ name: 'index' }" class="flex items-center">
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            AALP
-          </span>
+          <img width="50" height="50" src="/assets/images/logo.png" alt="Aalp Logo">
         </NuxtLink>
         <div class="flex items-center lg:order-2">
           <NuxtLink href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
