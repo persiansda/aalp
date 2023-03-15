e14f3a56af4ffbbdde4b87fb0aaab22c81a04bf7
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-15 09:02:08 +0300

    feat: scroll to top component

---
+++ components/aalp/ScrollToTop.vue
@@ -0,0 +1,16 @@
+<script  lang="ts" setup>
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+</script>
+
+<template>
+  <div class="fixed bottom-4 right-4 z-999">
+    <button class="bg-blue-500 hover:bg-blue-700 transition-all text-white py-2 px-3 rounded-full" @click="scrollToTop">
+      <Icon name="tabler:chevron-up" class="w-4 h-4 mb-1" />
+    </button>
+  </div>
+</template>
