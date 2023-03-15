b1b7a15bfb146bab75ef527c41dc96b9fbac36ce
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-15 08:49:43 +0300

    fix: fixed cntainer size

--- components/aalp/IconCarousel.vue
+++ components/aalp/IconCarousel.vue
@@ -9,16 +9,16 @@ const modules = [Autoplay]
 </script>
 
 <template>
-  <div class="container mx-auto bg-white drop-shadow-xl p-8 rounded-xl -m-10 z-999 relative">
-    <div class="px-10 flex">
-      <div class="fade-shadow right" />
+  <div class="container mx-auto px-28">
+    <div class="icon-carousel bg-white drop-shadow-xl py-8 rounded-xl -m-10 z-999 relative">
+      <div class="fade-shadow rounded-xl right" />
       <Swiper
         :modules="modules"
         :slides-per-view="8"
         :loop="true"
         :autoplay="{ delay: 0, disableOnInteraction: false }"
-        :allow-touch-move="false"
         :scrollbar="{ draggable: true }"
+        :allow-touch-move="false"
         :free-mode="false"
         :speed="1300"
       >
@@ -28,34 +28,36 @@ const modules = [Autoplay]
           </div>
         </SwiperSlide>
       </Swiper>
-      <div class="fade-shadow left" />
+      <div class="fade-shadow rounded-xl left" />
     </div>
   </div>
 </template>
 
 <style lang="scss">
-.swiper-wrapper{
-  transition-timing-function: linear;
-}
-
-.fade-shadow {
-  &.left {
-    background: linear-gradient(to left,hsla(0,0%,100%,0) 0,#fff 100%);
-    left: 0;
-    width: 20%;
+.icon-carousel {
+  .swiper-wrapper {
+    transition-timing-function: linear;
   }
 
-  &.right {
-    background: linear-gradient(to right,hsla(0,0%,100%,0) 0,#fff 100%);
-    right: 0;
-    width: 20%;
-  }
+  .fade-shadow {
+    &.left {
+      background: linear-gradient(to left, hsla(0, 0%, 100%, 0) 0, #fff 100%);
+      left: 0;
+      width: 20%;
+    }
 
-  bottom: 0;
-  height: 100%;
-  pointer-events: none;
-  position: absolute;
-  top: 0;
-  z-index: 9
+    &.right {
+      background: linear-gradient(to right, hsla(0, 0%, 100%, 0) 0, #fff 100%);
+      right: 0;
+      width: 20%;
+    }
+
+    bottom: 0;
+    height: 100%;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    z-index: 9
+  }
 }
 </style>
