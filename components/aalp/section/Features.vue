7a453accac7d22f2e5745cf27282889036610c79
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-17 17:57:19 +0300

    feat: sections base

---
+++ components/aalp/section/Features.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="relative py-32 bg-white overflow-hidden">
+    <!-- <img class="absolute left-0 top-0" src="flaro-assets/images/features/elipse.svg" alt=""> -->
+    <div class="container px-4 mx-auto">
+      <div class="flex items-center">
+        <div class="w-full md:w-1/2 p-8 relative">
+          <img class="" src="https://demo.persiansda.com/assets/images/about-frame.png" alt="">
+          <div class="absolute top-1/4 -right-60 w-full h-full rounded-2xl">
+            <img src="https://demo.persiansda.com/assets/images/about-screen.png" alt="">
+            <!-- transform hover:-translate-y-16 transition ease-in-out duration-1000 -->
+          </div>
+        </div>
+        <div class="w-full md:w-1/2">
+          <h2 class="mb-6 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight">
+            Build better customer experiences with Flaro
+          </h2>
+          <p class="font-sans text-lg text-gray-900 leading-relaxed md:max-w-lg">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
