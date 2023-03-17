b3488d6a168b7051abe03780923e2c8a3b10ad64
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-17 20:50:02 +0300

    fix: added props

--- components/aalp/section/Features.vue
+++ components/aalp/section/Features.vue
@@ -1,22 +1,55 @@
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  highlight: {
+    type: String,
+    default: null,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  secondaryImage: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  reverse: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+})
+</script>
+
 <template>
   <section class="relative py-32 bg-white overflow-hidden">
-    <!-- <img class="absolute left-0 top-0" src="flaro-assets/images/features/elipse.svg" alt=""> -->
     <div class="container px-4 mx-auto">
-      <div class="flex items-center">
+      <div class="md:flex items-center justify-center">
         <div class="w-full md:w-1/2 p-8 relative">
-          <img class="" src="https://demo.persiansda.com/assets/images/about-frame.png" alt="">
-          <div class="absolute top-1/4 -right-60 w-full h-full rounded-2xl">
+          <img :src="image" :alt="`${highlight ?? ''} ${title}`">
+          <div v-if="secondaryImage" class="absolute top-1/4 right-28 rounded-2xl">
             <img src="https://demo.persiansda.com/assets/images/about-screen.png" alt="">
             <!-- transform hover:-translate-y-16 transition ease-in-out duration-1000 -->
           </div>
         </div>
-        <div class="w-full md:w-1/2">
-          <h2 class="mb-6 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight">
-            Build better customer experiences with Flaro
-          </h2>
-          <p class="font-sans text-lg text-gray-900 leading-relaxed md:max-w-lg">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat.
-          </p>
+        <div class="w-full md:w-1/3">
+          <div class="block">
+            <h2 class="mb-6 text-primary text-5xl font-bold font-heading tracking-px-n leading-tight">
+              <span v-if="highlight" class="text-secondary">{{ highlight }}</span> {{ title }}
+            </h2>
+            <p class="text-lg">
+              {{ description }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
