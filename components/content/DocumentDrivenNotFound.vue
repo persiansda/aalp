467773c1d451a2e61bd8f405a46c97f1e80e7d64
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-21 11:46:47 +0300

    fix: custom ddnf

---
+++ components/content/DocumentDrivenNotFound.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+useHead({
+  title: '404 - Page not found',
+  meta: [
+    {
+      name: 'description',
+      content: 'This is not the page you are looking for.',
+    },
+  ],
+})
+</script>
+
+<template>
+  <div class="p-48">
+    <AError code="404" title="Page not found" message="This is not the page you're looking for." icon="uil:file" />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
